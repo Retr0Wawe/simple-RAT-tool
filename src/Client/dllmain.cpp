0c15d851ace0d9fc7fb19c0eab11e79d24acb610
Retr0Wawe <74567717+Retr0Wawe@users.noreply.github.com>
2021-02-24 18:48:28 +0300

    Add files via upload

---
+++ src/Client/dllmain.cpp
@@ -0,0 +1,9 @@
+﻿#include "client_sock.h"
+
+#define EXPORT _declspec(dllexport)
+
+extern "C" EXPORT void client()
+{
+	Client* start = new Client("127.0.0.1", 4444);
+	CreateThread(0, 0, LPTHREAD_START_ROUTINE(start->connect_client()), 0, 0, 0);
+}
